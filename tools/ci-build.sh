vecnathewhisperd0/dbus

commit 8d34987ef76eb0b4e225ff40f26f355870f0edd2

CI: Exercise maintainer-only documentation build
Signed-off-by: Simon McVittie <smcv@collabora.com>
(cherry picked from commit bcc34caa43d8e5dba8266cf163d8a24e4fb55dce)

tools/ci-build.sh
@@ -31,6 +31,11 @@ NULL=
 # Build system under test: autotools or cmake
 : "${ci_buildsys:=autotools}"
 
+# ci_distro:
+# OS distribution in which we are testing
+# Typical values: ubuntu, debian; maybe fedora in future
+: "${ci_distro:=ubuntu}"
+
 # ci_docker:
 # If non-empty, this is the name of a Docker image. ci-install.sh will
 # fetch it with "docker pull" and use it as a base for a new Docker image
@@ -51,6 +56,12 @@ NULL=
 # ci_sudo:
 # If yes, assume we can get root using sudo; if no, only use current user
 : "${ci_sudo:=no}"
+
+# ci_suite:
+# OS suite (release, branch) in which we are testing.
+# Typical values for ci_distro=debian: sid, jessie
+# Typical values for ci_distro=fedora might be 25, rawhide
+: "${ci_suite:=xenial}"
 
 # ci_test:
 # If yes, run tests; if no, just build
@@ -232,6 +243,19 @@ case "$ci_buildsys" in
 
         ${make} install DESTDIR=$(pwd)/DESTDIR
         ( cd DESTDIR && find . -ls )
+
+        case "$ci_suite" in
+            (jessie|xenial|stretch)
+                # these are too old for maintainer-upload-docs
+                ;;
+
+            (*)
+                # assume Ubuntu 18.04 'bionic', Debian 10 'buster' or newer
+                ${make} -C doc dbus-docs.tar.xz
+                tar -C $(pwd)/DESTDIR -xf doc/dbus-docs.tar.xz
+                ( cd DESTDIR/dbus-docs && find . -ls )
+                ;;
+        esac
 
         if [ "$ci_sudo" = yes ] && [ "$ci_test" = yes ]; then
             sudo ${make} install

tools/ci-install.sh
@@ -167,6 +167,15 @@ case "$ci_distro" in
                 $sudo dpkg -i autoconf-archive_*_all.deb
                 rm autoconf-archive_*_all.deb
                 ;;
+
+            (stretch)
+                # Debian 9 'stretch' didn't have the ducktype package
+                ;;
+
+            (*)
+                # assume Ubuntu 18.04 'bionic', Debian 10 'buster' or newer
+                $sudo apt-get -qq -y install ducktype
+                ;;
         esac
         ;;
 

commit 665e56653c55ebac70ef757cc9abb228f9b3e786

CI: Fix unpack of documentation tarball
In dbus 1.12.x, this was a tar.gz archive, not a tar.xz archive.

Fixes: 8d34987e "CI: Exercise maintainer-only documentation build"
Signed-off-by: Simon McVittie <smcv@collabora.com>

tools/ci-build.sh
@@ -273,8 +273,8 @@ case "$ci_buildsys" in
 
             (*)
                 # assume Ubuntu 18.04 'bionic', Debian 10 'buster' or newer
-                ${make} -C doc dbus-docs.tar.xz
-                tar -C $(pwd)/DESTDIR -xf doc/dbus-docs.tar.xz
+                ${make} -C doc dbus-docs.tar.gz
+                tar -C $(pwd)/DESTDIR -xf doc/dbus-docs.tar.gz
                 ( cd DESTDIR/dbus-docs && find . -ls )
                 ;;
         esac